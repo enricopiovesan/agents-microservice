587a723adb4aa0301b07a21d087d1db1ee3b04ca
Giulia <giulia@Giulias-MacBook-Pro.local>
2021-10-12 23:26:00 -0400

    controllers - add agent controller unit tests

---
+++ src/controllers/edit-agent.spec.js
@@ -0,0 +1,54 @@
+import makeEditAgent from './edit-agent'
+import makeFakeAgent from '../../__test__/fixtures/agent'
+
+describe('edit agent controller', () => {
+  it('successfully edite an agent', async () => {
+    const fakeAgent = makeFakeAgent()
+    const editAgent = makeEditAgent({ editAgent: a => a })
+    const request = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params: {
+        id: fakeAgent.id
+      },
+      body: fakeAgent
+    }
+    const expected = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Last-Modified': new Date(fakeAgent.modifiedOn).toUTCString()
+      },
+      statusCode: 200,
+      body: { edited: request.body }
+    }
+    const actual = await editAgent(request)
+    expect(actual).toEqual(expected)
+  })
+  it('reports user errors', async () => {
+    const fakeAgent = makeFakeAgent()
+    const editAgent = makeEditAgent({
+      editAgent: () => {
+        throw Error('Agent not modify')
+      }
+    })
+    const request = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params: {
+        id: fakeAgent.id
+      },
+      body: fakeAgent
+    }
+    const expected = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      statusCode: 400,
+      body: { error: 'Agent not modify'}
+    }
+    const actual = await editAgent(request)
+    expect(actual).toEqual(expected)
+  })
+})
