f4cd5722999f46dc335295bac3291b27e2f674c5
Giulia <giulia@Giulias-MacBook-Pro.local>
2021-10-13 12:35:34 -0400

    restApi - add basic endpoints

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import {
+  deleteAgent,
+  getAgents,
+  notFound,
+  createAgent,
+  editAgent
+} from './controllers'
+import makeCallback from './express-callback'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(bodyParser.json());
+const apiRoot = process.env.DM_API_ROOT
+const app = express()
+app.use(bodyParser.json())
+app.use((_, res, next) => {
+  res.set({ Tk: '!' })
+  next()
+})
+app.post(`${apiRoot}/agents`, makeCallback(createAgent))
+app.delete(`${apiRoot}/agents/:id`, makeCallback(deleteAgent))
+app.delete(`${apiRoot}/agents`, makeCallback(deleteAgent))
+app.patch(`${apiRoot}/agents/:id`, makeCallback(editAgent))
+app.patch(`${apiRoot}/agents`, makeCallback(editAgent))
+app.get(`${apiRoot}/agents`, makeCallback(getAgents))
+app.use(makeCallback(notFound))
 
-// listen for requests
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+  console.log('Server is listening on port 3000')
+})
 
-export default app;
+
+export default app
